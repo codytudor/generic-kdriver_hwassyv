01cbb815cb3279eaf70131b37a1de1bc0267d7d4
Cody <cody.tudor@gmail.com>
2016-01-27 11:25:36 -0800

    updated comments

--- hwassyv.c
+++ hwassyv.c
@@ -1,13 +1,19 @@
 /*
  * Generic HW/ASSY Version Reporting Driver
  *
- * Copyright 2015 Tudor Design Systems, LLC.
+ * Copyleft 2016 Tudor Design Systems, LLC.
  *
  * Author: Cody Tudor <cody.tudor@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  */
 
